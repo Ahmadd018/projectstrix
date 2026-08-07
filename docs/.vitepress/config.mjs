987b5fcc6242b90bab3b41224f46642b01865ab7
Ali <aliq766667@gmail.com>
2026-08-07 16:47:32 +0400

    Fix: Update top nav Documentation link to point to new folder structure

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Documentation', link: '/DESCRIPTION' }
+      { text: 'Documentation', link: '/guide/what-is-strix' }
     ],
 
     sidebar: [
